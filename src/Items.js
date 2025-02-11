82491a87d277ff034560114836b05d9736b73f84
nicoo111 <jcncreyes@mymail.mapua.edu.ph>
2025-02-11 19:07:29 +0800

    chck

--- src/Items.js
+++ src/Items.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
-import AddToCartBtn from "./AddToCartBtn";
 import "./Items.css";
+import ProductDetailsModal from "./ProductDetailsModal";
 import fetchMenuData from "./fetchMenuData";
+
 
 const Items = () => {
   const [menuItems, setMenuItems] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null); 
+  const [isModalOpen, setIsModalOpen] = useState(false); 
 
   useEffect(() => {
     const getMenuData = async () => {
       const data = await fetchMenuData();
       setMenuItems(data);
-
       const uniqueCategories = [...new Set(data.map((item) => item.item_category))];
       setCategories(uniqueCategories);
     };
@@ -19,11 +21,20 @@ const Items = () => {
     getMenuData();
   }, []);
 
+  const openModal = (item) => {
+    setSelectedItem(item);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedItem(null);
+  };
+
   return (
     <div className="container">
       {categories.map((category, index) => (
         <div key={index} className="category-container">
-          
           <h3 className="category">{category}</h3>
           <div className="items-group">
             {menuItems
@@ -31,18 +42,14 @@ const Items = () => {
               .map((item) => (
                 <div key={item.item_id} className="item-card">
                   <div className="item-details">
-                  <img src={`images/${item.item_id}.png`} alt={item.item_name} className="item-image" />
-                  {/*  <img src="./5" alt={item.item_name} className="item-image" />*/}
+                    <img src={`images/${item.item_id}.png`} alt={item.item_name} className="item-image" />
                     <div className="text-content">
                       <h2 className="item-name">{item.item_name}</h2>
                       <p className="eta">{item.item_preptime_min}-{item.item_preptime_max} minutes</p>
                       <p className="description">{item.item_description}</p>
                       <div className="bottom-row">
-                        <p className="price">
-                          Php {item.item_price.toFixed(2)}
-                        </p> 
-                        
-                        <AddToCartBtn />
+                        <p className="price">Php {item.item_price.toFixed(2)}</p>
+                        <button className="add-to-cart-button" onClick={() => openModal(item)}>+</button>
                       </div>
                     </div>
                   </div>
@@ -52,6 +59,8 @@ const Items = () => {
           <hr />
         </div>
       ))}
+
+      <ProductDetailsModal isOpen={isModalOpen} closeModal={closeModal} selectedItem={selectedItem} />
     </div>
   );
 };
--- src/fetchMenuData.js
+++ src/fetchMenuData.js
@@ -2,6 +2,7 @@ import supabase from "./SupabaseClient";
 
 const fetchMenuData = async () => {
   try {
+
     const { data: menuData, error: menuError } = await supabase
       .from("menu")
       .select("*");
@@ -15,13 +16,30 @@ const fetchMenuData = async () => {
       throw new Error(`Error fetching menu sizes: ${menuSizesError.message}`);
 
 
+    const { data: menuAddonsData, error: menuAddonsError } = await supabase
+      .from("menu_addons")
+      .select("*");
+    if (menuAddonsError)
+      throw new Error(`Error fetching menu addons: ${menuAddonsError.message}`);
+
     const combinedData = menuData.map((menuItem) => {
-      const matchingSizes = menuSizesData
-        .filter((size) => size.item_id === menuItem.item_id)
-        .map((size) => size.size_price);
-    
-      const minPrice = matchingSizes.length > 0 ? Math.min(...matchingSizes) : 0;
-    
+      const matchingSizes = menuSizesData.filter(
+        (size) => size.item_id === menuItem.item_id
+      );
+
+      const minPrice =
+        matchingSizes.length > 0
+          ? Math.min(...matchingSizes.map((size) => size.size_price))
+          : 0;
+
+      const matchingAddons = menuAddonsData
+        .filter((addon) => addon.item_id === menuItem.item_id)
+        .map((addon) => ({
+          addon_id: addon.addon_id,
+          addon_name: addon.addon_name,
+          addon_price: addon.addon_price,
+        }));
+
       return {
         item_id: menuItem.item_id,
         item_name: menuItem.item_name,
@@ -30,6 +48,7 @@ const fetchMenuData = async () => {
         item_preptime_max: menuItem.item_preptime_max,
         item_category: menuItem.item_category,
         item_price: minPrice,
+        addons: matchingAddons, 
       };
     });
 
@@ -37,7 +56,6 @@ const fetchMenuData = async () => {
 
     return combinedData;
   } catch (error) {
- 
     console.error("Error fetching menu data:", error.message);
     return [];
   }
