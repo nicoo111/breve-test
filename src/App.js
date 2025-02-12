752d59974f7330434049619e9ff8b05f31b41fea
Guillan Hojilla <guillanhojilla55@gmail.com>
2025-02-12 14:55:28 +0800

    Open cart front end

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './App.css';
 import Items from "./Items";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
   return (
     <div>
-      <div className="logo">
-        <h1>BrevéLine</h1>
+    <div className = "logo">
+    <h1>BrevéLine</h1>
       </div>
 
       <div>
         <Items />
       </div>
+      
+      <div>
+      <button className="floating-cart">
+      <FontAwesomeIcon icon={faCartShopping} className="cart-icon" />
+       </button>
+      </div>
     </div>
   );
 };
-
 
 export default App;
